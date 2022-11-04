95c847a080eeec9e8a9f85961e950dea389236be
Dima <adasd.s200@yandex.ru>
2022-11-04 10:33:21 +0300

    add midleware

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 require('@babel/register');
-
 const express = require('express');
+const ssr = require('./midleware/ssr');
 
 const app = express();
-
+app.use(express.static('public'));
+app.use(ssr);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 const PORT = 3000;
 
 app.listen(PORT, () => console.log('server working'));
