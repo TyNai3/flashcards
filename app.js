6990b56e80af48f304e720102ec18b70ad7cf9d0
Dima Melnikov <Miller-89@yandex.ru>
2022-11-04 14:49:38 +0300

    rega

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const mainRouter = require('./routes/mainRouter');
 const topicRouter = require('./routes/topicRouter');
 const questionsRouter = require('./routes/questionRouter');
 
+
+
 app.use(express.static('public'));
 app.use(ssr);
 app.use(express.urlencoded({ extended: true }));
@@ -15,16 +17,15 @@ app.use(express.json());
 
 //* Подключить Роутеры
 const registrationRouter = require('./routes/render/registrationRouter');
-
+const registrationApiRouter = require('./routes/api/regRouter');
 //* Запустить роутеры
 
 app.use('/', mainRouter);
 app.use('/topics', topicRouter);
-
+app.use('/', registrationApiRouter);
 app.use('/registration', registrationRouter);
 
 app.use('/', questionsRouter);
-
 
 const PORT = 3000;
 
--- views/Layout.jsx
+++ views/Layout.jsx
@@ -1,21 +1,21 @@
 const React = require('react');
 const Header = require('./Header');
 
-function Layout({ children, user }) {
+function Layout({ children, user, title }) {
   return (
-   
+
     <html lang="en">
       <head>
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="stylesheet" href="/css/bootstrap.min.css" />
-        <title>Company</title>
+        <title>{title}</title>
       </head>
       <body>
         <Header user={user} />
         {children}
-  
+
       </body>
     </html>
   );
