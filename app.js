5a929a0a4b3ee286475682280d8fbcbed755a6c9
Dmitry Melnikov <108584355+TMillers@users.noreply.github.com>
2022-11-04 15:14:35 +0300

    Revert "rega"

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ const mainRouter = require('./routes/mainRouter');
 const topicRouter = require('./routes/topicRouter');
 const questionsRouter = require('./routes/questionRouter');
 
-
-
 app.use(express.static('public'));
 app.use(ssr);
 app.use(express.urlencoded({ extended: true }));
@@ -17,15 +15,16 @@ app.use(express.json());
 
 //* Подключить Роутеры
 const registrationRouter = require('./routes/render/registrationRouter');
-const registrationApiRouter = require('./routes/api/regRouter');
+
 //* Запустить роутеры
 
 app.use('/', mainRouter);
 app.use('/topics', topicRouter);
-app.use('/', registrationApiRouter);
+
 app.use('/registration', registrationRouter);
 
 app.use('/', questionsRouter);
+
 
 const PORT = 3000;
 
