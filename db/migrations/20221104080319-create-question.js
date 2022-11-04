125ce05dc356f7584279ba0ebef88864554f38ae
Dima Melnikov <Miller-89@yandex.ru>
2022-11-04 11:40:40 +0300

    create DB

---
+++ db/migrations/20221104080319-create-question.js
@@ -0,0 +1,39 @@
+const topic = require('../models/topic');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Questions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.TEXT,
+      },
+      answer: {
+        type: Sequelize.TEXT,
+      },
+      topic_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Topics',
+          key: 'id',
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Questions');
+  },
+};
