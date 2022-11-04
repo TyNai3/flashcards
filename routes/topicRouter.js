fe539dce565014520d247ca727a886a121ebba97
Roman <roman.shemelin31@gmail.com>
2022-11-04 11:39:43 +0300

    add router for topic questions

--- routes/topicRouter.js
+++ routes/topicRouter.js
@@ -1,12 +1,40 @@
 const router = require('express').Router();
-const { Topic } = require('../db/models');
-
 const TopicPage = require('../views/TopicPage');
+const { Topic } = require('../db/models');
+const { Question } = require('../db/models');
 
 router.get('/', async (req, res) => {
-  const topics = await Topic.findAll({
-    logging: false,
-  });
-  res.renderComponent(TopicPage, { topics });
+  try {
+    const topics = await Topic.findAll();
+    res.renderComponent(TopicPage, { topics });
+  } catch (error) {
+    console.log(error.message);
+  }
 });
+
+router.get('/:topic_id', async (req, res) => {
+  try {
+    const { topic_id } = req.params;
+    const topic = await Topic.findOne({ where: { id: topic_id } });
+    const question = await Question.findAll({ where: { topic_id } });
+    const question_id = question[0].id;
+    res.redirect(`/${topic_id}/questions/${question_id}`);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+router.post('/:topic_id/:questionId', async (req, res) => {
+  try {
+    const { questionId } = req.params;
+    console.log(req.params);
+    const question = await Question.findOne({ where: { id: questionId } });
+    // console.log(question);
+    res.json(question);
+    res.end();
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
 module.exports = router;
